c8194524a6b533a42c1bd8cb4d645ad59f81f5ca
synzen <michael994664@gmail.com>
2020-03-14 22:28:35 -0400

    Add winston for proper logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import sequelize from './database/sequelize';
 import syncDatabase from './database/syncDatabase';
+import log from './util/log';
 
 syncDatabase(sequelize, true).then(() => {
+  log.debug('Database synced');
   // Proceed to boot up Skynet
 });
---
+++ src/util/log.ts
@@ -0,0 +1,42 @@
+import { createLogger, transports, format } from 'winston';
+
+// By default, log to two different files
+const log = createLogger({
+  level: 'info', // defines the minimum level that should be logged
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+  ),
+  transports: [
+    new transports.File({
+      /**
+       * Anything below error level will not be logged
+       */
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new transports.File({
+      /**
+       * Since the default level is info, anything below level
+       * (including debug) will not be logged to combined
+       */
+      filename: 'logs/combined.log',
+    }),
+  ],
+});
+
+// If environment isn't production, log to console
+if (process.env.NODE_ENV !== 'production') {
+  /**
+   * Only console transport will log debug
+   */
+  log.add(new transports.Console({
+    level: 'debug',
+    format: format.combine(
+      format.timestamp(),
+      format.prettyPrint(),
+    ),
+  }));
+}
+
+export default log;
