ff7cd6613000df38646e224f7d60e90809582796
synzen <michael994664@gmail.com>
2020-03-15 12:49:07 -0400

    Add winston for proper logging (#6)
    * Add winston for proper logging
    
    * Funnel sequelize logs to winston

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from 'sequelize';
 import config from '../config/config';
+import log from '../util/log';
 
 const { uri } = config;
 
-export default new Sequelize(uri);
+export default new Sequelize(uri, {
+  logging: (sqlString) => log.debug(sqlString),
+});
--- src/database/syncDatabase.ts
+++ src/database/syncDatabase.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import ProductKey from '../models/ProductKey';
+import log from '../util/log';
 
 /**
  * Initialize all models and create relevant tables if they
@@ -11,6 +12,7 @@ import ProductKey from '../models/ProductKey';
 const sync = async (sequelize: Sequelize, force = false) => {
   ProductKey.initialize(sequelize);
 
+  log.debug(`Creating all necessary tables. dropping existing tables: ${force}`);
   await sequelize.sync({
     force,
   });
