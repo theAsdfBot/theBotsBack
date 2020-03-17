46241c8936ed44454f4199486b5a68ccca88fdf8
synzen <michael994664@gmail.com>
2020-03-16 23:28:57 -0400

    Set up client TS/React/ESLint (#10)
    Changed the eslint script to add double quotes - without double quotes, the behavior [varies by OS](https://eslint.org/docs/user-guide/command-line-interface) which is why the root eslint was linting the client and causing errors (even though .eslintignore exists).

---
+++ src/client/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript',
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'linebreak-style': 'off'
+  }
+};
