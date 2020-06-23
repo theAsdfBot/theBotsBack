9b2157ea1c073697d4f8b3b56717233d37108f39
Michael T <michael994664@gmail.com>
2020-06-23 12:27:27 -0400

    Add product key renewal service (#20)
    * Add jest watch script

---
+++ src/services/productKeys/renew.ts
@@ -0,0 +1,16 @@
+import ProductKey from '../../models/ProductKey';
+
+export default async (id: string, days: number) => {
+  const productKey = await ProductKey.findOne({
+    where: {
+      id,
+    },
+  });
+  if (!productKey || !productKey.expireAt) {
+    throw new Error('Missing product key or product key expireAt');
+  }
+  const ms = 1000 * 60 * 60 * 24 * days;
+  await productKey.update({
+    expireAt: new Date(productKey.expireAt.getTime() + ms),
+  });
+};
---
+++ src/services/productKeys/unit_renew.test.ts
@@ -0,0 +1,21 @@
+import renew from './renew';
+import ProductKey from '../../models/ProductKey';
+
+jest.mock('../../models/ProductKey');
+
+describe('Unit::services/productKeys/renew', () => {
+  it('throws an error if no expireAt found', async () => {
+    jest.spyOn(ProductKey, 'findOne')
+      .mockResolvedValue({
+        expireAt: null,
+      } as unknown as ProductKey);
+    await expect(renew('d', 1)).rejects
+      .toThrow('Missing product key or product key expireAt');
+  });
+  it('throws an error if no product key found', async () => {
+    jest.spyOn(ProductKey, 'findOne')
+      .mockResolvedValue(null as unknown as ProductKey);
+    await expect(renew('d', 1)).rejects
+      .toThrow('Missing product key or product key expireAt');
+  });
+});
